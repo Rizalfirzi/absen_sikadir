84695b4a80ecfdab4a6971d62c8cd1bdd5786733
ikbalmiftahudin15 <ikbalmiftahudin15112005@gmail.com>
2023-09-22 08:51:14 +0700

    import

---
+++ app/Http/Controllers/ImportKehadiranController_LOCAL_1722.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Import_kehadiran;
+use Illuminate\Http\Request;
+
+class ImportKehadiranController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        return view('admin.import_pegawai.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $file = $request->file('file');
+        $fileName = time().rand(1,100).'.'.$file->extension();
+        $file->move(resource_path('views/admin/import_pegawai/files'));
+        return response()->json(['succes'=>$fileName]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Import_kehadiran $import_kehadiran)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Import_kehadiran $import_kehadiran)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Import_kehadiran $import_kehadiran)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Import_kehadiran $import_kehadiran)
+    {
+        //
+    }
+}
